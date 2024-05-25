1e4c002ff1d862aec18f472aa53eebc460a527a8
Shaun Harris <shaun@shaunwilliamharris.com>
2024-05-25 17:20:06 +0100

    notes removed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-//Event listerner to set man function to run when HTML doc is loaded 
-//elements needed for button to display form when adding a new dog and the main container to display dog/s
 document.addEventListener("DOMContentLoaded", () => {
     const formButton = document.querySelector(".dogs-list__button--add")
     const dogContainer = document.querySelector(".main")
-  
-    // Creates a list item for a dog and sets up a click event listener to display the dog's card when clicked
-    //output needed is a list item that when click event happens show the dogs details 
+
     function createDogListItem(dog) {
       const listItem = document.createElement("li")
       listItem.className = "dogs-list__button"
@@ -19,15 +15,12 @@ document.addEventListener("DOMContentLoaded", () => {
       return listItem
     }
   
-    // Creates a section element with a specific class name
     function createSection() {
       const section = document.createElement("section")
       section.className = "main__dog-section"
       return section
     }
   
-    // Creates a description section for a dog card
-    //needs to include header for bio as well as bio
     function createDogCardDescription(bio) {
       const descriptionDiv = document.createElement("div")
       descriptionDiv.className = "main__dog-section__desc"
@@ -43,14 +36,6 @@ document.addEventListener("DOMContentLoaded", () => {
       return descriptionDiv
     }
   
-    // Creates the bottom section of a dog card, including a button and status text
-    //function with dog object
-    //create the elements needed <button, "p" text and the footer div
-    //assign button to footer element (style purposes)
-    //content for good/bad dog
-    //t/f statment to support good/bad dog functionality
-    //click event listener to alter between the two.
-    //append to footer and return the footer 
     function createDogCardFooter(dog) {
       const button = document.createElement("button")
       const statusText = document.createElement("p")
@@ -71,7 +56,6 @@ document.addEventListener("DOMContentLoaded", () => {
       return footerDiv
     }
   
-    // Create the entire dog card, assembling the header, image, description, and footer sections
     function createDogCard(dog) {
       const section = createSection()
       const header = document.createElement("h2")
@@ -88,13 +72,11 @@ document.addEventListener("DOMContentLoaded", () => {
       return section
     }
   
-    // Set up the form button to clear the main container and display the form for adding a new dog
     formButton.addEventListener("click", () => {
       dogContainer.innerHTML = ""
       dogContainer.appendChild(renderAddDogForm())
     })
   
-    // Render the form for adding a new dog, including the header and the form elements
     function renderAddDogForm() {
       const section = createSection()
       const form = createForm()
@@ -106,8 +88,7 @@ document.addEventListener("DOMContentLoaded", () => {
   
       return section
     }
-  
-    // Create the form for adding a new dog and sets up the submit event listener to add the dog to the list
+
     function createForm() {
       const form = document.createElement("form")
   
@@ -150,8 +131,7 @@ document.addEventListener("DOMContentLoaded", () => {
       )
       return form
     }
-  
-    // Create an input element, setting type, id, and name attributes, and optionally setting a value
+
     function createInput(id, type = "text", value) {
       let input
   
@@ -171,7 +151,6 @@ document.addEventListener("DOMContentLoaded", () => {
       return input
     }
   
-    // Create a label element and sets its attributes
     function createLabel(forAttr, text) {
       const label = document.createElement("label")
       label.htmlFor = forAttr
@@ -180,7 +159,6 @@ document.addEventListener("DOMContentLoaded", () => {
       return label
     }
   
-    // Render the list of dogs by creating list items for each dog and appending them to the list container
     function renderDogList(dogs) {
       const listContainer = document.querySelector(".dogs-list")
       for (const dog of dogs) {
@@ -188,8 +166,7 @@ document.addEventListener("DOMContentLoaded", () => {
         listContainer.append(listItem)
       }
     }
-  
-    // Call renderDogList to display the initial list of dogs
+ 
     renderDogList(data)
   })
   
